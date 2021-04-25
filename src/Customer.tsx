0ed89cffb473e48f191d75ae5ac4c88a472b7d80
Adilet312 <adiletm@hotmail.com>
2021-04-25 13:29:44 -0700

    Add UnitStorage App With TS and React.

---
+++ src/Customer.tsx
@@ -0,0 +1,72 @@
+export default class Customer{
+  private fullName: string;
+  private phoneNumber: string;
+  private accountBalance: number;
+  
+  constructor(fullName:string, phoneNumber:string, accountBalance:number){
+    this.fullName = fullName;
+    this.phoneNumber = phoneNumber;
+    this.accountBalance = accountBalance;
+  }
+
+  getFullName():string{
+    return this.fullName;
+  }
+
+  getPhoneNumber():string{
+    return this.phoneNumber;
+  }
+
+  getAccountBalance():number{
+    return this.accountBalance;
+  }
+
+  setFullName(givenName:string):void{
+    this.fullName = givenName;
+  }
+
+  setPhoneNumber(givenNumber:string):void{
+    this.phoneNumber = givenNumber;
+  }
+
+  setAccountBalance(givenAmount:number):void{
+    if(!this.isValidAmount(givenAmount)){
+      throw 'Amount is invalid!';
+    }
+    this.accountBalance = givenAmount;
+  }
+
+  payBalance(amount:number):void{
+    if(amount > this.getAccountBalance()){
+       throw 'Given amount is bigger than current balance!';
+    }else if(!this.isValidAmount(amount)){
+       throw 'Amount is invalid!';
+    }
+    let currentBalance = this.getAccountBalance() - amount;
+    this.setAccountBalance(currentBalance)
+  }
+
+  depositBalance(amount:number):void{
+    if(!this.isValidAmount(amount)){
+       throw 'Amount is invalid!';
+    }
+    let currentBalance = this.getAccountBalance() + amount;
+    this.setAccountBalance(currentBalance)
+  }
+
+  isValidAmount(amount:number):boolean{
+    if(amount > 0){
+      return true;
+    }
+    return false;
+  }
+
+  toString():string{
+    let description = "";
+    description += "Customer name: " + this.getFullName() + "\n";
+    description += "Phone Number: " + this.getPhoneNumber() + "\n";
+    description += "Account Balance: " + this.getAccountBalance() + "\n";
+    description += "\n";
+    return description;
+  }
+}
---
+++ src/StorageUnit.tsx
@@ -0,0 +1,106 @@
+interface Customer {
+  fullName: string;
+  phoneNumber: string;
+  accountBalance: number;
+}
+
+export enum UnitType{ STANDARD = 'STANDARD', HUMID_CONTROL = 'HUMID_CONTROL', TEMP_CONTOL = 'TEMP_CONTOL' }
+
+export default class StorageUnit{
+  private width:number;
+  private height: number;
+  private length: number;
+  private price: number;
+  private type: UnitType;
+  private customer?: Customer;
+  private date?: Date;
+
+  constructor(width: number, height: number, length: number, price: number, type: UnitType, customer?:any, date?:Date){
+    this.width = width;
+    this.height = height;
+    this.length = length;
+    this.price = price;
+    this.type = type;
+    if(customer!==null){
+      this.customer = customer;
+      this.date = date;
+    }
+
+  }
+
+  setWidth(givenWidth:number):void{
+    this.width = givenWidth;
+  }
+
+  setLength(givenLength:number):void{
+    this.length = givenLength
+  }
+
+  setHeight(givenHeight:number):void{
+    this.height = givenHeight;
+  }
+
+  setPrice(givenPrice:number):void{
+    this.price = givenPrice;
+  }
+
+  setCustomer(newCustomer:Customer):void{
+    this.customer = newCustomer;
+  }
+
+  setUnitType(unitType:UnitType):void{
+    this.type = unitType;
+  }
+
+  setDate(date:Date): void{
+    this.date = date;
+  }
+
+  getWidth():number{
+    return this.width;
+  }
+
+  getHeight(): number{
+    return this.height;
+  }
+
+  getLength(): number{
+    return this.length;
+  }
+
+  getPrice(): number{
+    return this.price;
+  }
+
+  getCustomer(): any{
+    if(this.customer===null){
+      return null;
+    }
+    return this.customer;
+  }
+
+  getUnitType(): UnitType{
+    return this.type;
+  }
+
+  getDate(): Date | any{
+    if(this.date===null){
+      return null;
+    }
+    return this.date;
+  }
+
+  toString():string{
+    let description = "";
+    description += "Type of Storage Unit: " + this.getUnitType() + ",";
+    description += "Dimensions of Storage Unit (WxLxH): " + this.getWidth() + " x " + this.getLength() + " x " + this.getHeight() + ",";
+    if (this.customer != null){
+        description += "Storage Unit Rented by: "+ this.getCustomer().getFullName() + ",";
+        description += "Starting Rental Date: " + this.getDate().toDateString();
+    }
+    else {
+        description += "Storage Unit is Unrented";
+    }
+    return description;
+  }
+}
